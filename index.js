0e23ea595788d5a14cc6a1a0b2debdfb1950bca4
orenw <oren.weinberg@cellebrite.com>
2022-04-12 16:00:33 +0300

    loops instead hardcode

--- index.js
+++ index.js
@@ -14,98 +14,77 @@
     },
 
     onAdd: function(map) {
-      if ((this.options && this.options.maps.length > 0) || this.options.overlays.length > 0) {
-        var layerGroup = L.layerGroup();
+      if ((this.options)){
+        var overlays;
+        var baseLayers;
 
-        if (this.options.overlays.length > 0) {
-          var overlays = {};
+        //layers
+        if (this.options.overlays) {
+          overlays = {};
 
-          if (this.options.overlays.includes('statesOverlay')) {
-            // State overLayer
-            var statesOverlay = esri.featureLayer({
-              url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3',
-              style: function() {
-                ({ color: '#bada55', weight: 2 });
+          var layerName;
+          for(var layer of this.options.overlays){
+            try{
+              layerName = layer.name;
+              var overlay;
+              if(layer.type === "featureLayer"){
+                const cloneStyle = { ...layer.style };
+
+                overlay = esri.featureLayer({
+                  url: layer.url,
+                  simplifyFactor: layer.simplifyFactor,
+                  precision: layer.precision,
+                  style: function() {
+                    return cloneStyle;
+                  }
+                });
+              }else if(layer.type === "vectorTileLayer"){
+                overlay = vector.vectorTileLayer(
+                  layer.url
+                );
               }
-            });
 
-            overlays['U.S. States'] = statesOverlay;
-          }
-
-          if (this.options.overlays.includes('electionResultOverLay')) {
-            // State electionResultOverLay
-            var electionResultOverLay = esri.featureLayer({
-              url:
-                'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Congressional_Districts/FeatureServer/0',
-              simplifyFactor: 0.5,
-              precision: 5,
-              style: function(feature) {
-                if (feature.properties.PARTY === 'Democrat') {
-                  return { color: 'blue', weight: 2 };
-                } else if (feature.properties.PARTY === 'Republican') {
-                  return { color: 'red', weight: 2 };
-                } else {
-                  return { color: 'white', weight: 2 };
-                }
-              }
-            });
-
-            overlays['U.S. Election results'] = electionResultOverLay;
+              overlays[layer.name] = overlay;
+            }catch(error){
+              console.log(`ERROR, add layer - ${layerName}, ${error}`);
+            }
           }
         }
-		
-		    var vectorTileMicrosoft = vector.vectorTileLayer(
-              'https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Microsoft_Building_Footprints/VectorTileServer'
-            );
-			overlays['AAA'] = vectorTileMicrosoft;
 
-        if (this.options.maps.length > 0) {
-          var baseLayers = {
-            basic: layerGroup.addTo(map)
-          };
+        // maps
+        if (this.options.maps) {
+          var mapName;
+          for(var newMap of this.options.maps){
+            try{
+              if(!baseLayers){
+                var layerGroup = L.layerGroup();
+                baseLayers = {
+                  basic: layerGroup.addTo(map)
+                };
+              }
 
-          if (this.options.maps.includes('Streets')) {
-            baseLayers['Streetmap'] = esri.basemapLayer('Streets');
+              mapName = newMap.name;
+
+              var tileProtocol = newMap.tileProtocol;
+              var newMapObj = {
+                urlTemplate: tileProtocol + newMap.config.urlTemplate,
+                options: newMap.config.options
+              }
+
+              baseLayers[mapName] = esri.basemapLayer(newMapObj);
+            }catch(error){
+              console.log(`ERROR, add map - ${mapName}, ${error}`);
+            }
           }
+        }
 
-          if (this.options.maps.includes('Gray')) {
-            baseLayers['Grayscale'] = esri.basemapLayer('Gray');
-          }
+        if(baseLayers || overlays){
+          L.control.layers(baseLayers, overlays).addTo(map);
 
-          if (this.options.maps.includes('NationalGeographic')) {
-            baseLayers['NationalGeographic'] = esri.basemapLayer('NationalGeographic');
-          }
+          var container = L.DomUtil.create('div', 'cb-esri');
 
-          if (this.options.maps.includes('DarkGray')) {
-            baseLayers['DarkGray'] = esri.basemapLayer('DarkGray');
-          }
-
-          if (this.options.maps.includes('GrayLabels')) {
-            baseLayers['GrayLabels'] = esri.basemapLayer('GrayLabels');
-          }
-		  
-		  if (this.options.maps.includes('USATopo')) {
-			//Example to add map directly for offline mode using local server
-			var tileProtocol = 'http:';//(window.location.protocol !== 'https:') ? 'http:' : 'https:';
-			let USATopo =  {
-			urlTemplate: tileProtocol + '//{s}.arcgisonline.com/ArcGIS/rest/services/USA_Topo_Maps/MapServer/tile/{z}/{y}/{x}',
-				options: {
-					minZoom: 1,
-					maxZoom: 15,
-					subdomains: ['server', 'services'],
-					attribution: 'USGS, National Geographic Society, i-cubed'
-				}
-			}          
-			baseLayers['USATopo'] = esri.basemapLayer(USATopo);
-          }
-        }		
-
-        L.control.layers(baseLayers, overlays).addTo(map);
-
-        var container = L.DomUtil.create('div', 'cb-esri');
-        // container.style.backgroundColor = 'green';
-
-        return container;
+          return container;
+        }
       }
     }
   })
@@ -113,6 +92,4 @@
   L.control.cbEsri = function(options) {
     return new L.Control.CbEsri(options);
   };
-
 }));
-
